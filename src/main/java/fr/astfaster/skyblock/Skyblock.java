1d9081dae319c432164d6dcc24143047e77b66a5
AstFaster <astfaster@gmail.com>
2021-06-03 07:26:00 +0200

    Initial commit: base project

---
+++ src/main/java/fr/astfaster/skyblock/Skyblock.java
@@ -0,0 +1,17 @@
+package fr.astfaster.skyblock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Skyblock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+    
+}
